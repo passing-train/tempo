5dbae6296d342ce0b5445399a0f870c42359bf68
Pim Snel <pim@lingewoud.nl>
2017-11-29 02:12:10 +0100

    code cleanup

--- app/ask.rb
+++ app/ask.rb
@@ -58,22 +58,10 @@ class Ask
 
   def ask
     picked = PROMPTS[rand*PROMPTS.length]
-
-#    answer = input(picked, @last_answer)
     input_window(picked, @last_answer)
-
-    #p answer
-
-    #if answer
-      #@last_answer = answer
-      #log(@last_answer)
-    #end
   end
 
   def set_new_answer(answer)
-#    @new_answer = answer
-    p answer
-
     if answer
       @last_answer = answer
       log(@last_answer)
@@ -82,62 +70,10 @@ class Ask
 
   def input_window(prompt, default_value="")
     @ask_window_controller ||= AskWindowController.alloc.init_with_arguments(self)
-
     @ask_window_controller.set_prompt prompt
     @ask_window_controller.set_default_value default_value
-
-
-#    @alert.accessoryView = @input_field
-#    @alert.window.collectionBehavior = NSWindowCollectionBehaviorCanJoinAllSpaces
-#    @alert.window.level = NSFloatingWindowLevel
-#    @alert.window.setInitialFirstResponder(@input_field)
-#    @alert.window.makeFirstResponder(@input_field)
-#    buttonClicked = @alert.runModal
-
-#    @input_field.tableViewDelegate = self
-#    @input_field.stringValue if buttonClicked == 1000
-
     @ask_window_controller.showWindow(self)
     @ask_window_controller.window.orderFrontRegardless
-  end
-
-  def input(prompt, default_value="")
-    @alert = NSAlert.alloc.init
-    @alert.addButtonWithTitle("Add entry")
-    @alert.addButtonWithTitle("Cancel")
-
-    @okbutton = @alert.buttons.objectAtIndex 0
-    @oldTarget = @okbutton.target
-    @oldAction = @okbutton.action
-
-    @okbutton.setTarget self
-    @okbutton.setAction "verifyPopover:"
-    @alert.setMessageText(prompt)
-
-    @input_field = WuAutoCompleteTextField.alloc.initWithFrame(NSMakeRect(0, 0, 200, 24))
-    @input_field.awakeFromNib
-    @input_field.stringValue = default_value
-
-    @input_field.tableViewDelegate = self
-    @input_field.delegate = self
-
-    @alert.accessoryView = @input_field
-    @alert.window.collectionBehavior = NSWindowCollectionBehaviorCanJoinAllSpaces
-    @alert.window.level = NSFloatingWindowLevel
-    @alert.window.setInitialFirstResponder(@input_field)
-    @alert.window.makeFirstResponder(@input_field)
-    buttonClicked = @alert.runModal
-
-    @input_field.tableViewDelegate = self
-    @input_field.stringValue if buttonClicked == 1000
-  end
-
-  def verifyPopover notification
-    unless @input_field.autoCompletePopover.isShown
-      @okbutton.setTarget @oldTarget
-      @okbutton.setAction @oldAction
-      @okbutton.performClick(@alert)
-    end
   end
 
   def textField(textField, completions:somecompletions, forPartialWordRange:partialWordRange, indexOfSelectedItem:theIndexOfSelectedItem)
